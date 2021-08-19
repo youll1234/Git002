2146756d2dfb201f66c0d8632e3aa4775a9a5866
huancghao <429083636@qq.com>
2021-08-19 10:58:23 +0800

    1-chao-commit

--- src/main/java/com/chao/git/MyGit.java
+++ src/main/java/com/chao/git/MyGit.java
@@ -5,5 +5,6 @@ public class MyGit {
         System.out.println("实打实大苏打-001");
         System.out.println("weweweas-002");
         System.out.println("erefgsew-003");
+        System.out.println("tiyttnrkjke-004");
     }
 }
